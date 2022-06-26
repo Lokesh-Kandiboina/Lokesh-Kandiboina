64b7fa66618ae40441a44831b3fbd09c4906dc97
Mr.Loki <Mr.Loki@DESKTOP-HGM8MOF>
2022-06-26 11:29:48 -0800

    Integer

---
+++ SVKPDemo/src/Integer.java
@@ -0,0 +1,26 @@
+
+public class Integer {
+
+	public static void main(String[] args) {
+		
+		int max = 1439567247;
+		int min = -1439567248;
+		short shortMax = 25677;
+		short shortMin = -25678;
+		long longMax = 540348543567L;
+		long longMin = -345682865246L;
+		byte byteMax = 127;
+		byte byteMin = -128;
+		
+		System.out.println("Integer max value is : " + max);
+		System.out.println("Integer min value is : " + min);
+		System.out.println("Integer shortMax value is : " + shortMax);
+		System.out.println("Integer shortMin value is : " + shortMin);
+		System.out.println("Integer longMax value is : " + longMax);
+		System.out.println("Integer longMin value is : " + longMin);
+		System.out.println("Integer byteMax value is : " + byteMax);
+		System.out.println("Integer byteMin value is : " + byteMin);
+
+	}
+
+}
