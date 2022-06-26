55e30af685cc05593417ea7c31e336de1227a623
Mr.Loki <Mr.Loki@DESKTOP-HGM8MOF>
2022-06-26 11:30:10 -0800

    Boolean

---
+++ SVKPDemo/src/Boolean.java
@@ -0,0 +1,13 @@
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		boolean var = false; //true or false(0 or 1)
+		System.out.println(var);
+		
+		char vari = '\u00A7';//$,*,.,
+		System.out.println(vari);
+
+	}
+
+}
