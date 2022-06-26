de92ed5c2a4b656852bc8e9960594a28e73de6b7
Mr.Loki <Mr.Loki@DESKTOP-HGM8MOF>
2022-06-26 11:30:27 -0800

    Float

---
+++ SVKPDemo/src/Float.java
@@ -0,0 +1,20 @@
+
+public class Float {
+
+	public static void main(String[] args) {
+		/*
+		 int value1 = 7/2;
+		 float value2 = 11f/5f;
+		 double value3 = 11f/5f;
+		 System.out.println("value 1 = " + value1);
+		 System.out.println("value 2 = " + value2);
+		 System.out.println("value 3 = " + value3);
+		 */
+	 int marker = 450;
+	 double percentage = marker * 0.40f;
+	 
+	 System.out.println("percentage : " + percentage);
+
+	}
+
+}
