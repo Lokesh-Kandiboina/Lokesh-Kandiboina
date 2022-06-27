69a125868797c74addc9b578ea2035924e1656eb
Mr.Loki <Mr.Loki@192.168.0.102>
2022-06-27 10:52:31 -0800

    Assignment

--- SVKPDemo/src/Code.java
+++ SVKPDemo/src/Code.java
@@ -2,13 +2,13 @@
 public class Code {
 
 	public static void main(String[] args) {
-      for(int i=1;i<=5;i++)
+      for(int i=1;i<=7;i++)
       {
-    	  for(int j=1;j<=i;j++)
+    	  for(int j=1;j<=i; j++)
     	  {
-    		  System.out.print('@');
+    		  System.out.print("@ ");
     	  }
-    	  System.out.println();
+    	  System.out.println( );
       }
 	}
 
