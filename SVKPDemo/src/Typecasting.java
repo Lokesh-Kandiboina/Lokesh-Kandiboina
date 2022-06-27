2bb24d267fca87911ec6e9c360f7db4e5e342b30
Mr.Loki <Mr.Loki@192.168.0.102>
2022-06-27 12:38:47 -0800

    TypeCasting

--- SVKPDemo/src/Typecasting.java
+++ SVKPDemo/src/Typecasting.java
@@ -2,8 +2,11 @@
 public class Typecasting {
 
 	public static void main(String[] args) {
-		int number = 50; 
-		float point = number;
+		int number = 10;
+		long l = (long)number;
+		double d2 = 1.234e2;
+		float point = (float)d2;
+        System.out.println(l);
         System.out.println(point);
 	}
 
