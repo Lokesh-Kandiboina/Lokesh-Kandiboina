4c099e50ced13473ef03ddf9c84c92e5dfaa8508
Mr.Loki <Mr.Loki@DESKTOP-HGM8MOF.bbrouter>
2022-06-29 12:52:59 -0800

    Example Scanner

--- SVKPDemo/src/com/svkp/scannerbuffer/ScannerDemo.java
+++ SVKPDemo/src/com/svkp/scannerbuffer/ScannerDemo.java
@@ -7,10 +7,11 @@ public class ScannerDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Enter a number : ");
-		Scanner obj = new Scanner(System.in);
-		int x;
-		x = obj .nextInt();
-		System.out.println("The entered number is : " + x);
+		try (Scanner obj = new Scanner(System.in)) {
+			int x;
+			x = obj .nextInt();
+			System.out.println("The entered number is : " + x);
+		}
 
 	}
 
