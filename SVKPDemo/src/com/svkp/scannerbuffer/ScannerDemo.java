0be1a6747508e22970ab610b3c2514de119b4ca7
Mr.Loki <Mr.Loki@DESKTOP-HGM8MOF.bbrouter>
2022-06-29 12:40:37 -0800

    Example Scanner

---
+++ SVKPDemo/src/com/svkp/scannerbuffer/ScannerDemo.java
@@ -0,0 +1,17 @@
+package com.svkp.scannerbuffer;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a number : ");
+		Scanner obj = new Scanner(System.in);
+		int x;
+		x = obj .nextInt();
+		System.out.println("The entered number is : " + x);
+
+	}
+
+}
