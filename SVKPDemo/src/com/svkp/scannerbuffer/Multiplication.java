f4d9beb3a2302d011143424d18d81eaf43dea9d4
Mr.Loki <Mr.Loki@DESKTOP-HGM8MOF>
2022-07-14 22:46:35 +0530

    Multiplication

---
+++ SVKPDemo/src/com/svkp/scannerbuffer/Multiplication.java
@@ -0,0 +1,13 @@
+package com.svkp.scannerbuffer;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int num = 3;
+		for(int i = 1; i <= 20; ++i)
+		{
+			System.out.printf("%d * %d = %d \n" , num, i, num * i);
+		}
+	}
+
+}
