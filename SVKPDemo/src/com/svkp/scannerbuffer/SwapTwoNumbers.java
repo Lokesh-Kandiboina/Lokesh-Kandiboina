8dd4e99e83b4a4b5cb254013c91ee1ac4dc657b5
Mr.Loki <Mr.Loki@DESKTOP-HGM8MOF>
2022-07-14 22:05:59 +0530

    Swap Two Numbers

--- SVKPDemo/src/com/svkp/scannerbuffer/SwapTwoNumbers.java
+++ SVKPDemo/src/com/svkp/scannerbuffer/SwapTwoNumbers.java
@@ -1,23 +1,22 @@
 package com.svkp.scannerbuffer;
 
-import java.util.Scanner;
 
 public class SwapTwoNumbers {
 
 	public static void main(String[] args) {
-		try (Scanner in = new Scanner(System.in)) {
-			System.out.println("Input the first number: ");
-			double a = in.nextDouble();
-			System.out.println("Input the second number: ");
-			double b = in.nextDouble();
-			System.out.println("Input the third number: ");
-			double c = in.nextDouble();
-			System.out.println("The smallest value is " + smallest(a,b,c)+"\n");
-		}
+		int i = 120, j = 240;
+		System.out.println("Before Swap");
+		System.out.println("i = " + i);
+		System.out.println("j = " + j);
+		
+		int temp = i;
+		i = j;
+		j = temp;
+		
+		System.out.println("After Swap");
+		System.out.println("i = " + i);
+		System.out.println("j = " + j);
 	}
 
-	private static double smallest(double a, double b, double c) {
-		return Math.min(Math.min(a,b), c);
-	}
 
 }
