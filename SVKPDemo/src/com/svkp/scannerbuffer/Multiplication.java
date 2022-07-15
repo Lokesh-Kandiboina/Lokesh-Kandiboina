befc5abafcb7c55ce748fba5f8555a8b463a270a
Mr.Loki <Mr.Loki@192.168.0.104>
2022-07-15 11:58:45 +0530

    Multiplication

--- SVKPDemo/src/com/svkp/scannerbuffer/Multiplication.java
+++ SVKPDemo/src/com/svkp/scannerbuffer/Multiplication.java
@@ -4,7 +4,7 @@ public class Multiplication {
 
 	public static void main(String[] args) {
 		int num = 3;
-		for(int i = 1; i <= 20; ++i)
+		for(int i = 1; i <= 10; ++i)
 		{
 			System.out.printf("%d * %d = %d \n" , num, i, num * i);
 		}
