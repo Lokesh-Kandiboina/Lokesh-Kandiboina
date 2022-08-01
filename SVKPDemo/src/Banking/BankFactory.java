8041e51940273be6aa1dc0dd112c288d12153fd8
Mr.Loki <Mr.Loki@DESKTOP-KDS845G>
2022-08-01 22:16:35 +0530

    BankIng

---
+++ SVKPDemo/src/Banking/BankFactory.java
@@ -0,0 +1,8 @@
+package Banking;
+
+public interface  BankFactory {
+
+	public abstract MMSavingAcc getNewSavingAccount(int accNo,String accNm,float accbal,boolean isSalary);
+
+	public abstract CurrentAcc getNewCurrentAccount(int accNo,String accNm,float accbal,float CreditLimit);
+}
---
+++ SVKPDemo/src/Banking/Customer.java
@@ -0,0 +1,19 @@
+package Banking;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		MMBankFactory mmbank=new MMBankFactory();
+		mmbank.getNewSavingAccount(0, null, 0, false);
+		mmbank.getNewCurrentAccount(3456, "Loki",4000, 60);
+		
+		MMSavingAcc mmsaving=new MMSavingAcc(456,"Nag's",5678,true);
+		mmsaving.Withdraw(2000);
+		
+		MMCurrentAcc mmcurrent=new MMCurrentAcc (143,"Mady",6543,420);
+		mmcurrent.Withdraw(500);
+		
+		mmsaving.toString();
+		mmcurrent.toString();
+	}
+}
